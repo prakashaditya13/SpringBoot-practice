7974b1b1ba8a5d5ac1bae6c282e6730846c4b57a
Aditya Prakash <prakashaditya13@yandex.com>
2021-12-03 21:28:20 +0530

    Add Path variable to endpoint

--- restful-web-services/src/main/java/com/aditya/rest/webservice/restfulwebservices/controller/SocialController.java
+++ restful-web-services/src/main/java/com/aditya/rest/webservice/restfulwebservices/controller/SocialController.java
@@ -1,13 +1,14 @@
 package com.aditya.rest.webservice.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SocialController {
 
-	@GetMapping(path = "/hello")
-	public String helloworld() {
-		return "Hello World";
+	@GetMapping(path = "/hello/{name}")
+	public String helloworld(@PathVariable String name) {
+		return "This is "+name;
 	}
 }
